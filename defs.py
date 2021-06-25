91273a3a78ed2c102c84e1ac7de0da7ef105b585
alex <alex.pereira@planejamento.gov.br>
2021-06-25 18:36:01 -0300

    added

--- defs.py
+++ defs.py
@@ -11,7 +11,7 @@ ES_USER = os.environ.get('ES_USER') or 'avnadmin'
 ES_PASSWORD = os.environ.get('ES_PASSWORD') or 'pass'  ## Setar Variavel de Ambiente
 ES_USE_SSL = os.environ.get('ES_USE_SSL') == "True"
 ES_VERIFY_CERTS = os.environ.get('ES_VERIFY_CERTS') == "True"
-KIBANA_HOST = os.environ.get('KIBANA_HOST') or 'k.vacinei.org'
+KIBANA_HOST = os.environ.get('KIBANA_HOST') or 'esvacinei-alex-5fd1.aivencloud.com'
 KIBANA_PORT = os.environ.get('KIBANA_PORT') or '443'
 
 if ES_USE_SSL:
@@ -20,6 +20,7 @@ if ES_USE_SSL:
     ES_URL = "https://{0}".format(ES_HOST)
 else:
     APP_PORT = 5000
+    PROTOCOL = 'http'
     ES_URL = "http://{0}:{1}".format(ES_HOST, ES_PORT)
 ES_VACINEI_INDEX = os.environ.get('ES_ESOCIAL_INDEX') or 'vacinei__vacinei'
 job_vacinei = {"index": ES_VACINEI_INDEX, "settings": body_settings_vacinei, "namespace": "default"}
