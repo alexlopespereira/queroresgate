e4f5d913d78668d7fdf65da1ffb877030513b126
alex <alex.pereira@planejamento.gov.br>
2021-06-27 11:41:57 -0300

    added

--- static/alert/alert.js
+++ static/alert/alert.js
@@ -10,5 +10,13 @@ var alertmap = new L.Map('alertmap', {
     zoomControl: true
 })
 
-var rectangle = L.rectangle([  [-15.6801,-47.9292], [-15.8801,-47.7292]]);
-alertmap.addLayer(rectangle);
+var bounds = [[-15.6801,-47.9292], [-15.8801,-47.7292]];
+
+var rect = L.rectangle(bounds, {color: 'blue', weight: 1}).on('click', function (e) {
+    // There event is event object
+    // there e.type === 'click'
+    // there e.lanlng === L.LatLng on map
+    // there e.target.getLatLngs() - your rectangle coordinates
+    // but e.target !== rect
+    console.info(e);
+}).addTo(alertmap);
