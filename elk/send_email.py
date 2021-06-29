3aa6249a0937b9fe89d19892c5d8ff687bdab76f
alex <alex.pereira@planejamento.gov.br>
2021-06-28 23:24:45 -0300

    added

--- elk/send_email.py
+++ elk/send_email.py
@@ -9,7 +9,9 @@ def send_email(to, nreports, type_vacina, google_maps_url):
       <body>
         <p>Ola {to},<br>
            Informamos que <b>{nreports}</b> usuários reportaram que hoje há doses remanescentes da vacina <b>{type_vacina}</b> na sua região de interesse <a href="{google_maps_url}">aqui</a>  <br>
-           Recomendamos ligar no local de vacinação para validar a informação antes de comparecer.
+           Recomendamos ligar no local de vacinação para validar a informação antes de comparecer. <br>
+           
+           Você pode remover este alerta em <a href="https://vacinei.org/alerta">https://vacinei.org/alerta</a>  <br> 
         </p>
       </body>
     </html>
